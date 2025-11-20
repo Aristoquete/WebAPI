1c334adda27d3ac9a24f8bc4f3db89add15819c3
LUANA CRISTINI ARAUJO FRANCO <00120520@unisoacad.uniso.br>
2025-11-19 22:37:50 -0300

    Atividade API - Criar API e Unity

---
+++ Save-Load_Luana/Save-Load_WebAPI/Controllers/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using Save_Load_WebAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Save_Load_WebAPI.Controllers
+{
+    public class PlayerController : ControllerBase
+    {
+        public static List<Player> players = new()
+        {
+            new Player { Id = "1", Vida = 10, QuantidadeItens = 0, PosicaoX = 0, PosicaoY = 0, PosicaoZ = 0 },
+        };
+
+        [HttpGet]
+        [Route("api/Player")]
+        public IActionResult GetPlayers()
+        {
+            return Ok(players);
+        }
+
+        [HttpGet]
+        [Route("api/Player/{id}")]
+        //Faz com que identifique o Player e pegue todas as suas informações apenas usando o ID
+        public IActionResult GetPlayerByID(string id)
+        {
+            var player = players.FirstOrDefault(p => p.Id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            return Ok(player);
+        }
+
+        [HttpPost]
+        [Route("api/Player")]
+        //Adiciona um player novo na lista
+        public IActionResult AddPlayer([FromBody] Player newPlayer)
+        {
+            players.Add(newPlayer);
+            return Ok(newPlayer);
+        }
+
+        [HttpDelete]
+        [Route("api/Player/{id}")]
+        //Deleta o Player com base do seu ID
+        public IActionResult DeletePlayer(string id)
+        {
+            var player = players.FirstOrDefault(p => p.Id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            players.Remove(player);
+            return Ok();
+        }
+        [HttpPut]
+        [Route("api/Player/{id}")]
+        //Atualiza as informações do Player
+        public IActionResult UpdatePlayer(string id, [FromBody] Player playerAtt)
+        {
+            var player = players.FirstOrDefault(p => p.Id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            player.Vida = playerAtt.Vida;
+            player.QuantidadeItens = playerAtt.QuantidadeItens;
+            player.PosicaoX = playerAtt.PosicaoX;
+            player.PosicaoY = playerAtt.PosicaoY;
+            player.PosicaoZ = playerAtt.PosicaoZ;
+
+            return Ok(player);
+        }
+    }
+}
---
+++ Save-Load_Luana/Save-Load_WebAPI/Model/Player.cs
@@ -0,0 +1,13 @@
+﻿namespace Save_Load_WebAPI.Model
+{
+    public class Player
+    {
+        public string Id { get; set; }
+        public int Vida { get; set; }
+        public int QuantidadeItens { get; set; }
+        public float PosicaoX { get; set; }
+        public float PosicaoY { get; set; }
+        public float PosicaoZ { get; set; }
+
+    }
+}
